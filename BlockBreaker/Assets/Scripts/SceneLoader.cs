94b160960f43be68e90d557d27c94033c321bbaf
Phil Davidson <pxdavidson@msn.com>
2019-02-12 22:38:59 +0000

    BB_01
    Copied existing project from local repo to clones git repo so i can push changes.
    Tl;DR is I created a scene loader and 3 scenes. No gameplay or assets created yet.

---
+++ BlockBreaker/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Loads next scene in build index
+    public void LoadNextScene()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentScene+1);
+    }
+
+    // Loads first scene in build index
+    public void LoadFirstScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    // Quits application
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+}
