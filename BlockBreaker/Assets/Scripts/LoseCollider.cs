d47e1f3f49526d4ff949bcb5545c0cb06006494f
Phil Davidson <pxdavidson@msn.com>
2019-02-17 22:54:34 +0000

    BB_04A
    Added in Lose Collider object and associated script to have the game move to the game over screen .
    
    TODO: Resolve issues with C-sharp project and autocomplete.

---
+++ BlockBreaker/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseCollider : MonoBehaviour
+{
+    // Detect collision with trigger object and load GameOver scren. NOTE: String is hard coded.
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene("Game Over");
+    }
+}
