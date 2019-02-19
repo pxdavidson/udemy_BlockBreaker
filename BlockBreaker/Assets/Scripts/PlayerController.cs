26f591b559db2259fe2ee6fe4e2765cd128c5af5
Phil Davidson <pxdavidson@msn.com>
2019-02-19 00:11:32 +0000

    BB_05A
    Set up mouse to drive paddle position.
    Using Vector3 and transform.psoition in update in PlayerController.cs

---
+++ BlockBreaker/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Define Variables
+    [SerializeField] int wolrdUnitScreenSize = 16;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xMousePosInUnits = (Input.mousePosition.x / Screen.width * wolrdUnitScreenSize);
+        Vector3 paddlePos = new Vector3(xMousePosInUnits, transform.position.y, transform.position.z);
+        transform.position = paddlePos;
+    }
+}
