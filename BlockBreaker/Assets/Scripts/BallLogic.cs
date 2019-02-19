0aba1630ddc5cc7bf7f646670aa84e33e4a2dd43
Phil <Davidson>
2019-02-19 17:47:45 +0000

    BB_06A
    Made ball follow paddle in BallLogic.cs
    
    TODO: Why does it render with a Vector3 and not with a Vector2?

---
+++ BlockBreaker/Assets/Scripts/BallLogic.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class BallLogic : MonoBehaviour
+{
+    // Define Variables
+    [SerializeField] PlayerController paddle1;
+
+    // States
+    Vector3 ballVector;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballVector = transform.position - paddle1.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 paddlePos = paddle1.transform.position;
+        Vector3 ballPos = paddlePos + ballVector;
+        transform.position = ballPos;
+    }
+}
--- BlockBreaker/Assets/Scripts/PlayerController.cs
+++ BlockBreaker/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     // Define Variables
     [SerializeField] int wolrdUnitScreenSize = 16;
+    [SerializeField] float paddlePosXMin = 1f;
+    [SerializeField] float paddlePosXMax = 15f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,8 +18,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        PaddleControl();
+    }
+
+    private void PaddleControl()
+    {
         float xMousePosInUnits = (Input.mousePosition.x / Screen.width * wolrdUnitScreenSize);
-        Vector3 paddlePos = new Vector3(xMousePosInUnits, transform.position.y, transform.position.z);
+        Vector3 paddlePos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        paddlePos.x = Mathf.Clamp(xMousePosInUnits, paddlePosXMin, paddlePosXMax);
         transform.position = paddlePos;
     }
 }
