07a2247611d0d9361c0c8fc00e4965cfff672120
Phil Davidson <pxdavidson@msn.com>
2019-02-20 00:08:29 +0000

    BB_07A
    Added LaunchBall() to Ball.cs using if statements to manage isInPlay state.

--- BlockBreaker/Assets/Scripts/BallLogic.cs
+++ BlockBreaker/Assets/Scripts/BallLogic.cs
@@ -4,9 +4,11 @@ public class BallLogic : MonoBehaviour
 {
     // Define Variables
     [SerializeField] PlayerController paddle1;
+    [SerializeField] float yLaunchVector = 10f;
 
     // States
     Vector3 ballVector;
+    bool ballInPlay = false;
     
     // Start is called before the first frame update
     void Start()
@@ -16,6 +18,31 @@ public class BallLogic : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        if (!ballInPlay)
+        {
+            LockBall();
+            LaunchBall();
+        }
+    }
+
+    // Add force to ball
+    private void LaunchBall()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            float xLaunchVector = Random.Range(-2f, 2f);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xLaunchVector, yLaunchVector);
+            ballInPlay = true;
+        }
+        else
+        {
+            // Do nothing
+        } 
+    }
+
+    // Lock the ball to the paddle
+    private void LockBall()
     {
         Vector3 paddlePos = paddle1.transform.position;
         Vector3 ballPos = paddlePos + ballVector;
