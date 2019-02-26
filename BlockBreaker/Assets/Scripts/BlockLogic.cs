b48a8c428131b5224a17b460b39d04cb4eca6047
Phil Davidson <pxdavidson@msn.com>
2019-02-26 19:56:12 +0000

    BB13_A
    Added SFX to block collision using AudioSource.PlayClipAtPoint()
    Referenced the camera Vector 3 using Camera.main.transform.position
    Tidied the Destroy(gameObject) into it's own method.
    
    TODO: Test with audio.

--- BlockBreaker/Assets/Scripts/BlockLogic.cs
+++ BlockBreaker/Assets/Scripts/BlockLogic.cs
@@ -1,8 +1,26 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class BlockLogic : MonoBehaviour
-{
+{ 
+    // Define Variables
+    [SerializeField] AudioClip blockSFX;
+
+    // detect Collisio with Colliders
     private void OnCollisionEnter2D(Collision2D collision)
+    {
+        PlayBlockSFX();
+        Destroy();
+    }
+
+    // Plays the blockSFX. Triggered by collision
+    private void PlayBlockSFX()
+    {
+        AudioSource.PlayClipAtPoint(blockSFX, Camera.main.transform.position);
+    }
+
+    // Destroy GameObject
+    private void Destroy()
     {
         Destroy(gameObject);
     }
